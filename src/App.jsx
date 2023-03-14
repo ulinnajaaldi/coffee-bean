856c930ac04bd9ebddb06ae29d1f8bb4abfca74d
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-03-15 02:05:29 +0700

    feat: UI Base clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import React, { Suspense } from "react";
 import logo from "./assets/coffe-logo.svg";
+import shapeHero from "./assets/shape-hero.svg";
 import shapeTea from "./assets/shape-tea-cup.svg";
 import { BiShoppingBag } from "react-icons/bi";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FiArrowUpRight } from "react-icons/fi";
 import { ReactSVG } from "react-svg";
+import Marquee from "react-fast-marquee";
 const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
 export default function App() {
+  const Loading = () => {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <div className="inline-flex">
+          <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-orange-600"></div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="container max-w-7xl mx-auto">
+    <>
       <div className="relative z-40 pt-[22px] h-[100vh]">
-        <nav className="flex items-center justify-between">
+        <nav className="flex items-center justify-between px-16">
           <div className="flex items-center justify-center gap-2">
-            <ReactSVG src={logo} className="rotate-45" />
+            <img src={logo} alt="coffe-beans-logo" className="rotate-45" />
             <h1 className="text-2xl font-medium">CoffeBean</h1>
           </div>
           <div className="flex items-center justify-center gap-8">
@@ -21,9 +33,9 @@ export default function App() {
             <GiHamburgerMenu className="w-6 h-6" />
           </div>
         </nav>
-        <main className="flex items-center justify-center gap-[60px]">
+        <main className="container max-w-7xl mx-auto flex items-center justify-center gap-[60px] pt-[56px]">
           <section className="flex-1">
-            <h1 className="text-[68px] font-medium leading-none pt-[76px]">
+            <h1 className="text-[68px] font-medium leading-none ">
               PREMIUM COFFEE BEAN
             </h1>
             <p className="text-[20px] py-[12px]">
@@ -45,33 +57,36 @@ export default function App() {
             </div>
           </section>
           <section className="flex-1">
-            <svg
-              width="437"
-              height="501"
-              viewBox="0 0 437 501"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 200C0 89.543 89.543 0 200 0H237C347.457 0 437 89.5431 437 200V501H0V200Z"
-                fill="#FFB193"
-              />
-            </svg>
+            <div>
+              <img src={shapeHero} alt="shape-tea-pot" className="w-full" />
+            </div>
+            <Suspense fallback={<Loading />}>
+              <div className="absolute h-full w-full -top-12 right-1 xl:right-3 z-50">
+                <Spline scene="https://prod.spline.design/WBuUN4ma1K-jn2U2/scene.splinecode" />
+              </div>
+            </Suspense>
           </section>
           <section className="flex-1">
-            <div>
-              <ReactSVG src={shapeTea} />
+            <div className="w-full pb-7">
+              <div className="relative w-[90%] cursor-pointer">
+                <Suspense fallback={<Loading />}>
+                  <div className="absolute h-full w-full -top-7 right-4 z-50">
+                    <Spline scene="https://prod.spline.design/2hoJAt0qlEzdkFMw/scene.splinecode" />
+                  </div>
+                </Suspense>
+                <img src={shapeTea} alt="shape-tea-pot" />
+              </div>
             </div>
-            <div className="pb-5">
-              <h2 className="text-[30px] font-medium">
-                MAKE YOUR COFFEE LIKE A{" "}
+            <div className="pb-4">
+              <h2 className="text-[30px] font-medium leading-tight">
+                MAKE &nbsp; YOUR &nbsp; COFFEE &nbsp; LIKE &nbsp; A{" "}
                 <div className="text-white inline-block px-2 -rotate-6 bg-primary">
                   PRO
                 </div>
               </h2>
             </div>
             <ol className="list-outside list-decimal ml-5 flex flex-col gap-[13px]">
-              <li className="text-primary text-lg font-medium">
+              <li className="text-primary text-lg font-medium ">
                 <span className="text-black text-base font-normal">
                   Use high-quality, feshly roasted coffee beans.
                 </span>
@@ -95,25 +110,17 @@ export default function App() {
           </section>
         </main>
       </div>
-      <footer className="absolute py-[20px] w-full z-40 my-auto bg-[#EBA349] text-white bottom-0 left-0">
-        <div className="flex gap-5 text-lg font-medium">
-          <p>EXQUISITE COFFEE, EXCEPTIONAL QUALITY</p>
-          <p>+</p>
-          <p>PURE ENJOYMENT IN EVERY SIP</p>
-          <p>+</p>
-          <p>DISCOVER THE ART OF COFFEE </p>
-          <p>+</p>
-          <p>BEST COFFEE IN YOUR AREA</p>
-        </div>
+      <footer className="absolute py-[15px] w-full z-40 my-auto bg-[#EBA349] text-white bottom-0 left-0">
+        <Marquee gradient={false}>
+          <p className="text-lg font-medium">
+            EXQUISITE COFFEE, EXCEPTIONAL QUALITY&emsp;✦&emsp;PURE ENJOYMENT IN
+            EVERY SIP &emsp;✦&emsp;DISCOVER THE ART OF COFFEE&emsp;✦&emsp;BEST
+            COFFEE IN YOUR AREA&emsp;✦&emsp;COME AND CHILL&emsp;✦&emsp;
+          </p>
+        </Marquee>
       </footer>
-      {/* <footer className="relative h-screen"></footer> */}
       <div className="absolute h-screen w-1/2  bg-[#EAF0F1] z-0 top-0 left-0"></div>
       <div className="absolute h-screen w-1/2 bg-[#EAE8E4] z-0 top-0 right-0"></div>
-      {/* <Suspense fallback={""}>
-        <div className="absolute h-full w-full top-0 left-0 z-50">
-          <Spline scene="https://prod.spline.design/WBuUN4ma1K-jn2U2/scene.splinecode" />
-        </div>
-      </Suspense> */}
-    </div>
+    </>
   );
 }
