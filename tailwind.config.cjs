b9b6dca57a2dbb6b3f8f64aa3d1c17fa72611bd0
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-03-26 23:05:38 +0700

    feat: custom screen width

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      lg: "1270px",
+      xl: "1280px",
+      "2xl": "1440px",
+      "3xl": "2400px",
+    },
     extend: {
       colors: {
         primary: "#DA5935",
